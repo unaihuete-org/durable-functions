4efbb4d0d5f5b6dc74153c506eddb6fd4d4c7a60
Unai Huete Beloki <unhueteb@microsoft.com>
2020-10-28 13:47:17 +0100

    samples working

--- samples/precompiled/HttpStart.cs
+++ samples/precompiled/HttpStart.cs
@@ -14,7 +14,7 @@ namespace VSSample
     {
         [FunctionName("HttpStart")]
         public static async Task<HttpResponseMessage> Run(
-            [HttpTrigger(AuthorizationLevel.Function, methods: "post", Route = "orchestrators/{functionName}")] HttpRequestMessage req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, methods: "post", Route = "orchestrators/{functionName}")] HttpRequestMessage req,
             [DurableClient] IDurableClient starter,
             string functionName,
             ILogger log)
